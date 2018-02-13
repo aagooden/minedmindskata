da08affe60538353ac99756b701598d866b55518
Aaron Gooden <aagooden@me.com>
2018-02-13 12:30:44 -0500

    Added User Number Input

--- math_functions.rb
+++ math_functions.rb
@@ -7,10 +7,28 @@ def subtract(x,y)
 end
 
 def divide(x,y)
-	x / y
+	if x == 0 || y == 0
+		return 0
+	else
+		return x.to_f / y.to_f
+	end
+
 end
 
-p add(2,3)
-p subtract(5,3)
-p divide(10,5)
+def multiply(x,y)
+	x * y
+end
+
+p "Enter you first number"
+num1 = gets.chomp
+num1 = num1.to_f
+
+p "Enter your second number"
+num2 = gets.chomp
+num2 = num2.to_f
+
+p add(num1,num2)
+p subtract(num1,num2)
+p divide(num1,num2)
+p multiply(num1,num2)
 

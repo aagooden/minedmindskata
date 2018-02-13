2c53d3d48bc1f1c7997eed3bf816f181d32d0639
Aaron Gooden <aagooden@me.com>
2018-02-13 13:09:55 -0500

    Added calculator Functionality

--- math_functions.rb
+++ math_functions.rb
@@ -12,23 +12,41 @@ def divide(x,y)
 	else
 		return x.to_f / y.to_f
 	end
-
 end
 
 def multiply(x,y)
 	x * y
 end
 
-puts "Enter you first number"
-num1 = gets.chomp
-num1 = num1.to_f
+def enter()
+	puts "Enter you first number"
+	$num1 = gets.chomp
+	$num1 = $num1.to_f
 
-puts "Enter your second number"
-num2 = gets.chomp
-num2 = num2.to_f
+	puts "Operation?"
+	$op = gets.chomp
 
-puts "#{num1} + #{num2} = #{add(num1,num2)}"
-puts "#{num1} - #{num2} = #{subtract(num1,num2)}"
-puts "#{num1} / #{num2} = #{divide(num1,num2)}"
-puts "#{num1} * #{num2} = #{multiply(num1,num2)}"
+	puts "Enter your second number"
+	$num2 = gets.chomp
+	$num2 = $num2.to_f
+end
+
+loop do
+enter()
+
+if $op == "+"
+		puts "#{$num1} + #{$num2} = #{add($num1,$num2)}"
+	elsif $op == "-"
+		puts "#{$num1} - #{$num2} = #{subtract($num1,$num2)}"
+	elsif $op == "/"
+		puts "#{$num1} / #{$num2} = #{divide($num1,$num2)}"
+	elsif $op == "*"
+		puts "#{$num1} * #{$num2} = #{multiply($num1,$num2)}"
+	else
+		puts "Numbers or Operator Not Valid"
+end
+puts "\n"
+puts "Let's do it again"
+puts "\n"
+end
 

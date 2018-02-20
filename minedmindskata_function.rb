820f997ac80f64efa86f2bab38bde8ad54fe927b
Aaron Gooden <aagooden@me.com>
2018-02-20 15:58:42 -0500

    Gave 5 got minds test passed

--- minedmindskata_function.rb
+++ minedmindskata_function.rb
@@ -1,6 +1,8 @@
 def minedminds(x)
 	if x == 3
 		return "mined"
+	elsif x == 5
+		return "minds"
 	else
 		return x
 	end
--- test.minedmindskata.rb
+++ test.minedmindskata.rb
@@ -3,17 +3,25 @@ require_relative "minedmindskata_function.rb"
 
 class Addition_test < Minitest::Test
 
-def test_boolean
-	assert_equal(true, true)
-end
+	def test_boolean
+		assert_equal(true, true)
+	end
 
-def test_1_equals_1
-	assert_equal(1, minedminds(1))
-end
+	def test_1_equals_1
+		assert_equal(1, minedminds(1))
+	end
 
-def test_7_returns_7
-	assert_equal(7, minedminds(7))
-end
+	def test_7_returns_7
+		assert_equal(7, minedminds(7))
+	end
+
+	def test_3_returns_mined
+		assert_equal("mined", minedminds(3))
+	end
+
+	def test_5_returns_minds
+		assert_equal("minds", minedminds(5))
+	end
 
 
 end

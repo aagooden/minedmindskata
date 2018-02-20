3dc17714aa13655e81fafa8a0e7f1ce1a1284dff
Aaron Gooden <aagooden@me.com>
2018-02-20 15:19:04 -0500

    Boolean equals test working

--- test.minedmindskata.rb
+++ test.minedmindskata.rb
@@ -3,6 +3,10 @@ require_relative "minedmindskata_function.rb"
 
 class Addition_test < Minitest::Test
 
+def test_boolean
+	assert_equal(true, true)
+end
+
 def test_1_equals_1
 	assert_equal(1, minedminds(1))
 end

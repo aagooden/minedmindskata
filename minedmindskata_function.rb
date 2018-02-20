2eabe12e6795adde636edf9b837bfce7746aef5d
Aaron Gooden <aagooden@me.com>
2018-02-20 15:53:37 -0500

    Gave 3 got mined test passed

--- minedmindskata_function.rb
+++ minedmindskata_function.rb
@@ -1,3 +1,10 @@
 def minedminds(x)
-	return x
+	if x == 3
+		return "mined"
+	else
+		return x
+	end
 end
+
+
+
--- test.minedmindskata.rb
+++ test.minedmindskata.rb
@@ -11,4 +11,9 @@ def test_1_equals_1
 	assert_equal(1, minedminds(1))
 end
 
+def test_7_returns_7
+	assert_equal(7, minedminds(7))
+end
+
+
 end
